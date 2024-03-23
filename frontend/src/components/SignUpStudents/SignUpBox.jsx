892fbd853c6a23fd885a7a9e4694c39f4eae77aa
Thisara Chathuranga <129034261+thisaraChathuranga000@users.noreply.github.com>
2024-03-24 00:19:04 +0530

    new commits

--- frontend/src/components/SignUpStudents/SignUpBox.jsx
+++ frontend/src/components/SignUpStudents/SignUpBox.jsx
@@ -7,137 +7,67 @@ import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
-import axios from "axios";
-//import OutlinedInput from "@mui/material/OutlinedInput";
-//import ListItemText from "@mui/material/ListItemText";
-//import Checkbox from "@mui/material/Checkbox";
-
-// const ITEM_HEIGHT = 48;
-// const ITEM_PADDING_TOP = 8;
-// const MenuProps = {
-//   PaperProps: {
-//     style: {
-//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-//       width: 250,
-//     },
-//   },
-// };
-
-// const categoryList = [
-//   `Acounting & finance`,
-//   `Sales & Marketing`,
-//   `IT & Digital media`,
-//   `Labour`,
-//   `Other`,
-// ];
 
 function SignUpBox() {
-  const [userData, setuserData] = useState({
-    email: "",
-    password: "",
-    name: "",
-    contact: "",
-    uni_name: "",
-    res_address: "",
-    gender: "",
-    birthday: "",
-    age: "",
-  });
-
-  // const [categories, setCategories] = useState({
-  //   job_categories: [],
-  // });
-
-  const handleOnChange = (event) => {
-    setuserData({
-      ...userData,
-      [event.target.name]: event.target.value,
-    });
+  const [step, setStep] = useState(1);
+  const [selectedImage, setSelectedImage] = useState(null);
+ 
+  const nextStep = () => {
+    setStep(prevStep => prevStep + 1);
   };
 
-  // const handleCategoryChange = (event) => {
-  //   const {
-  //     target: { value },
-  //   } = event;
-  //   setCategories(
-  //     // On autofill we get a stringified value.
-  //     typeof value === "string" ? value.split(",") : value
-  //   );
-  // };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    console.log(userData);
-    axios
-      .post("http://localhost:5000/api/auth/register/undergraduates", userData)
-      .then((response) => {
-        console.log(response.data);
-        window.location.href = "/StdProfileInternal";
-         
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("Unsuccessful",);
-      });
-       
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedImage(file);
+    }
   };
+
   return (
     <div>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          pt: 5,
-          pl: 28,
-          marginBottom: 4,
-        }}
-      >
-        <Card
-          variant="outlined"
-          sx={{
-            boxShadow: "0 4px 8px #4A4A4A",
-            border: "1px solid #0069c4",
-            width: "400px",
-            pb: 4,
-          }}
-        >
+      <Box sx={{display: "flex",flexDirection: "column",pt: 5,pl: 28,marginBottom: 4,}}>
+        <Card variant="outlined" sx={{boxShadow: "0 4px 8px #4A4A4A",border: "1px solid #0069c4",width: "400px",pb: 4,}}>
           <CardContent sx={{ width: "340px", pl: 3.5 }}>
-            <div>
-              <Typography variant="h4" gutterBottom>
-                Sign Up as Undergraduate
-              </Typography>
-              
-              <form onSubmit={handleSubmit}>
+            {step === 1 ? (
+              <div>
+                <Typography variant="h4" gutterBottom>
+                  Sign Up
+                </Typography>
+
                 <TextField
                   label="Email"
                   variant="outlined"
                   fullWidth
                   margin="normal"
-                  name="email"
-                  onChange={handleOnChange}
                 />
+
                 <TextField
                   label="Create a Password"
                   type="password"
                   variant="outlined"
                   fullWidth
                   margin="normal"
-                  name="password"
-                  onChange={handleOnChange}
                 />
+                <Button type="submit" variant="contained" color="primary" fullWidth onClick={nextStep}>
+                  Next
+                </Button>
 
+                <Typography variant="body2" align="center" sx={{ pt: 2 }}>
+                  Allready Have an account?{" "}
+                  <Link href="#" color="primary">Log in</Link>
+                </Typography>
+              </div>
+            ) : step === 2 ? (
+              <div>
                 <TextField
                   label="Name"
                   variant="outlined"
                   fullWidth
                   margin="normal"
-                  name="name"
-                  onChange={handleOnChange}
                 />
 
                 <FormControl fullWidth>
@@ -146,13 +76,18 @@ function SignUpBox() {
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
                     label="Gender"
-                    name="gender"
-                    onChange={handleOnChange}
                   >
-                    <MenuItem value={'Male'}>Male</MenuItem>
-                    <MenuItem value={'female'}>Female</MenuItem>
+                    <MenuItem value={10}>Male</MenuItem>
+                    <MenuItem value={20}>Female</MenuItem>
                   </Select>
                 </FormControl>
+
+                <TextField
+                  label="Age"
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
+                />
 
                 <TextField
                   variant="outlined"
@@ -161,17 +96,6 @@ function SignUpBox() {
                   id="start-date"
                   helperText="Birthday"
                   type="date"
-                  name="birthday"
-                  onChange={handleOnChange}
-                />
-
-                <TextField
-                  label="Age"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  name="age"
-                  onChange={handleOnChange}
                 />
 
                 <TextField
@@ -179,8 +103,6 @@ function SignUpBox() {
                   variant="outlined"
                   fullWidth
                   margin="normal"
-                  name="contact"
-                  onChange={handleOnChange}
                 />
 
                 <TextField
@@ -188,8 +110,13 @@ function SignUpBox() {
                   variant="outlined"
                   fullWidth
                   margin="normal"
-                  name="uni_name"
-                  onChange={handleOnChange}
+                />
+
+                <TextField
+                  label="Permenent Address"
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
                 />
 
                 <TextField
@@ -197,99 +124,42 @@ function SignUpBox() {
                   variant="outlined"
                   fullWidth
                   margin="normal"
-                  name="res_address"
-                  onChange={handleOnChange}
                 />
 
-                {/* <div>
-                  <FormControl variant="outlined" fullWidth margin="normal">
-                    <InputLabel id="demo-multiple-checkbox-label">
-                      Preffered Job Categories
-                    </InputLabel>
-                    <Select
-                      labelId="demo-multiple-checkbox-label"
-                      id="demo-multiple-checkbox"
-                      multiple
-                      value={categories}
-                      onChange={handleCategoryChange}
-                      name="job_categories"
-                      input={<OutlinedInput label="preffered Job Categories" />}
-                      renderValue={(selected) => selected.join(", ")}
-                      MenuProps={MenuProps}
-                    >
-                      {categoryList.map((category) => (
-                        <MenuItem key={category} value={category}>
-                          <Checkbox
-                            checked={categories.indexOf(category) > -1}
-                          />
-                          <ListItemText primary={category} />
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </div> */}
-
-                {/* <div>
-                  <Typography variant="h6" gutterBottom>
-                    Upload Your Image
-                  </Typography>
-                  <Typography
-                    variant="subtitle2"
-                    gutterBottom
-                    sx={{ color: "gray" }}
-                  >
-                    Upload your Clear and Recent photo
-                  </Typography>
-                  <TextField
-                    fullWidth
-                    margin="normal"
-                    type="file"
-                    inputProps={{ accept: "image/*" }}
-                    onChange={handleImageChange}
-                    variant="outlined"
-                  />
-                </div>
-
-                <div sx={{ pt: 2 }}>
-                  <Typography variant="h6" gutterBottom>
-                    University Issued ID
-                  </Typography>
-                  <Typography
-                    variant="subtitle2"
-                    gutterBottom
-                    sx={{ color: "gray" }}
-                  >
-                    To keep our Community safe, Upload Your Sri lankan
-                    University Issued ID image that clearly shows your full name
-                    and complete photo
-                  </Typography>
-                  <TextField
-                    fullWidth
-                    margin="normal"
-                    type="file"
-                    inputProps={{ accept: "image/*" }}
-                    onChange={handleImageChange2}
-                    variant="outlined"
-                  />
-                </div> */}
-
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  fullWidth
+                <Button 
+                  type="submit" 
+                  variant="contained" 
+                  color="primary" 
+                  fullWidth sx={{marginTop:1}} 
+                  onClick={nextStep}
                 >
+                  next
+                </Button>
+              </div>
+            ) : (
+              <div>
+                <Typography variant="h6" gutterBottom>
+                  Upload Your Image
+                </Typography>
+
+                <Typography variant="subtitle2" gutterBottom sx={{ color: "gray" }}>
+                  Upload your Clear and Recent photo
+                </Typography>
+
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  type="file"
+                  inputProps={{ accept: "image/*" }}
+                  onChange={handleImageChange}
+                  variant="outlined"
+                />
+
+                <Button type="submit" variant="contained" color="primary" fullWidth>
                   Submit
                 </Button>
-              </form>
-
-              <Typography variant="body2" align="center" sx={{ pt: 2 }}>
-                Allready Have an account?{" "}
-                <Link href="/OrgProfileInternal" color="primary">
-                  Log in
-                </Link>
-              </Typography>
-            </div>
+              </div>
+            )}
           </CardContent>
         </Card>
       </Box>
