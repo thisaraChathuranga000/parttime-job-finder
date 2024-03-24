2ee21fb5f75665b73749b23552e2c36d05c16ba6
Thisara Chathuranga <129034261+thisaraChathuranga000@users.noreply.github.com>
2024-03-24 15:53:53 +0530

    new commits

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Res, UploadedFile, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/CreatePost.dto';
 import { UpdatePostDto } from './dto/UpdatePost.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('posts')
 export class PostsController {
@@ -9,13 +10,25 @@ export class PostsController {
 
     @Post()
     @UsePipes(new ValidationPipe())
-    createNewPost(@Body() createPostDto: CreatePostDto[]) {
+    @UseInterceptors(FileInterceptor('file'))
+    createNewPost(@Body() createPostDto: CreatePostDto, @UploadedFile() file) {
+        if (file) {createPostDto.img = file.filename}
         return this.postService.createPost(createPostDto)
+    }
+
+    @Get('all')
+    getAll(){
+        return this.postService.findAll()
     }
 
     @Get('all/:id')
     getAllPosts(@Param('id') id:string){
         return this.postService.findAllPosts(id)
+    }
+
+    @Get('uploads/:imgpath')
+    seeUploadedFile(@Param('imgpath') image, @Res() res){
+      return res.sendFile(image, {root: 'uploads/posts'});
     }
 
     @Get(':id')
